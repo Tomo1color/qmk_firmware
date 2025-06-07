5b4e87de91f13b0b23d6772a0f3fe21bf9e9bca9
Isshiki <isshiki@pajra.co.jp>
2025-06-07 15:18:14 +0900

    キーマップの修正

--- keyboards/salicylic_acid3/o51go/keymaps/tomo_ver2/keymap.c
+++ keyboards/salicylic_acid3/o51go/keymaps/tomo_ver2/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R, KC_T, KC_NO, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
         CTR2TB,   KC_A,   KC_S,    KC_D,    KC_F, KC_G, KC_NO, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_ENT,
         KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V, KC_B, KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_UP,   MT(MOD_RSFT, KC_SLSH),
-        KC_ENT,   KC_DEL, KC_LALT, KC_LGUI, LT(_ADJUST, KC_SPC), SF2SP, MO(_LOWER), MO(_RAISE), KC_LALT, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_ENT,   KC_DEL, KC_LALT, KC_LGUI, LT(_ADJUST, KC_SPC), SF2SP, SF2SP, MO(_RAISE), KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_LOWER] = LAYOUT(
@@ -87,9 +87,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_RAISE] = LAYOUT(
-      KC_GRV,     KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_NO, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    JP_YEN,
-        KC_LCTL,  KC_QUES,  KC_LABK, KC_RABK, KC_MINS, KC_EQL,  KC_NO, KC_COLN, KC_QUOT, KC_NO,   KC_LBRC, KC_RBRC, KC_PIPE,
-        _______,  KC_NO,    KC_NO,   KC_NO,   KC_UNDS, KC_PLUS, KC_NO, KC_NO,   KC_DQT,  _______, _______, KC_NO,   _______,
+        KC_GRV,   KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_NO, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    JP_YEN,
+        KC_LCTL,  KC_SLSH,  KC_DOT,  KC_COMM, KC_MINS, KC_EQL,  KC_NO, KC_COLN, KC_QUOT, KC_LBRC, KC_RBRC, KC_PIPE, KC_TILD,
+        _______,  KC_QUES,  KC_LABK, KC_RABK, KC_UNDS, KC_PLUS, KC_NO, KC_NO,   KC_DQT,  _______, _______, KC_NO,   _______,
         US_TO_JIS, _______, _______, _______, SFTSP,       KC_LSFT,  KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
     ),
 
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         JPZKHK,  KC_NO,  KC_F2,  KC_F3,   KC_F4,   KC_F5,  KC_NO, KC_F6,   KC_F7,   KC_F8,   KC_F9,   SCP,     KC_BSLS,
         CTR2F11, SF2F12, SGUI_L, GUI_BS,  KC_LGUI, KC_F10, KC_NO, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_MINS, _______,
         _______, CTL_Z,  CTL_X,  CTL_C,   CTL_V,   KC_NO,  KC_NO, KC_UNDS, KC_DEL,  KC_NO,   KC_NO,   KC_PGUP, KC_RSFT,
-        KC_NO,   KC_NO,  KC_NO,  QK_BOOT, KC_NO,     JIS_TO_US, CMDSP,       KC_BSPC,  KC_LALT, KC_HOME, KC_PGDN, KC_END
+      JIS_TO_US,   KC_NO,  KC_NO,  QK_BOOT, KC_NO,     KC_LSFT, CMDSP,       KC_BSPC,  KC_RALT, KC_HOME, KC_PGDN, KC_END
     )
 };
 
